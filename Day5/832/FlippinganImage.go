7aaa7d5bf10459f06a56b98fd3f1ada4b9cf7615
Prime38 <shoumikprime@gmail.com>
2019-10-12 00:54:30 +0600

    Day 5

--- Day5/832/FlippinganImage.go
+++ Day5/832/FlippinganImage.go
@@ -6,12 +6,7 @@ func flipAndInvertImage(A [][]int) [][]int {
 	for i:=0;i<len(A);i++{
 		l:=len(A[i])
 		for j:=0;j<(l+1)/2;j++{
-			t1:=A[i][j]
-			t2:=A[i][l-j-1]
-
-			A[i][l-j-1]=t1^1
-			A[i][j]=t2^1
-			//A[i][j],A[i][l-j-1]=A[i][l-j-1]^1,A[i][j]^1
+			A[i][j],A[i][l-j-1]=A[i][l-j-1]^1,A[i][j]^1
 		}
 	}
 	return A
